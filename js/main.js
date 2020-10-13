342100b54004b5b2fca8fc46824c976c6ac3c321
Massimiliano Ambrogio <maxi.ambrogio@gmail.com>
2020-10-13 19:10:24 +0200

    Project 14 fix html js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Parte js
+
+//Parte 1
+var userName = prompt('Quale è il tuo nome?');
+
+console.log('Nome User: ', userName);
+
+document.getElementById('name').innerHTML =  userName;
+
+//Parte 2
+var lastName = prompt('Quale è il tuo cognome?');
+
+console.log('Cognome User: ', lastName);
+
+document.getElementById('lastName').innerHTML = lastName;
+
+// Parte 3
+var color = prompt('Quale è il tuo colore preferito?');
+
+console.log('Colore preferito: ', color);
+
+document.getElementById('color').innerHTML = 'Classe 19 ' + color;
